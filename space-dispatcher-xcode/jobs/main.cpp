ac51e3438019b99ddc65e270548985fdfe22b62b
nckswt <nckswt@gmail.com>
2012-09-02 00:18:40 -0400

    added working GPIO code

---
+++ space-dispatcher-xcode/jobs/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jobs
+//
+//  Created by Nick Sweet on 2012-09-01.
+//  Copyright (c) 2012 Space Concordia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
