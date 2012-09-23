a5dd0b963e5ec4d32dd40a8ffe6c209d9b6e0e78
Siddhartha Kattoju <siddharthakattoju@gmail.com>
2012-09-22 22:20:33 -0300

    Update jobs/acs.cpp
    spelling error

--- jobs/acs.cpp
+++ jobs/acs.cpp
@@ -1,7 +1,7 @@
 /*
    Copyright 2012 Space Concordia
    Author: Nick Sweet
-   Subsystem Lead: Justin Jean-pierre
+   Subsystem Lead: Justin Jean-Pierre
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
